694d257e665d69afcfbf108b7566ff587e15a219
say-1241 <mrtadh166@gmail.com>
2026-07-21 16:22:12 +0300

    Implement voice channel join on bot ready
    Added voice channel joining functionality on bot ready event.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once('ready', () => {
+client.once('ready', async () => {
   console.log(`${client.user.tag} is online!`);
 
   client.user.setPresence({
@@ -16,6 +17,19 @@ client.once('ready', () => {
     ],
     status: 'online',
   });
+
+  const guild = client.guilds.cache.get('1077215115247636591');
+
+  if (!guild) return console.log('السيرفر غير موجود');
+
+  joinVoiceChannel({
+    channelId: '1200894234320965785',
+    guildId: '1077215115247636591',
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: false,
+  });
+
+  console.log('دخلت الروم الصوتي');
 });
 
 client.login(process.env.TOKEN);
